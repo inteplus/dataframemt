ad7d105ab24374d8332234557aeaec1a9104083b
Minh-Tri <pmtri80@gmail.com>
2019-12-01 07:53:26 +0000

    v0.4.0

---
+++ dataframemt/__init__.py
@@ -0,0 +1,3 @@
+from basemt import logger
+logger.debug_call_stack()
+logger.warning("Package 'dataframemt' is now renamed to package 'sqlmt'. Please update your code.")
--- setup.py
+++ setup.py
@@ -4,16 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='dataframemt',
-    version='0.3.9',
+    version='0.4.0',
     description="Extra Python modules to deal with dataframes for Minh-Tri Pham",
     author=["Minh-Tri Pham"],
     packages=find_packages(),
     install_requires=[
-        'sqlalchemy',  # for psql access
-        'tzlocal',  # for getting the local timezone
-        'psycopg2-binary',  # for psql access
-        # 'mysql', # for mysql access
-        'basemt',  # Minh-Tri's base modules for logging and multi-threading
-        'pandasmt',
+        'sqlmt>=0.3.9',
     ],
 )
